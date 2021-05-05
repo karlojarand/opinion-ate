74bb81778dc721646d7b34a5e81ebc8a5ff619db
karlojarand <55685955+karlojarand@users.noreply.github.com>
2021-05-05 18:49:15 +0300

    Final changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
+import React from 'react';
 import {Provider} from 'react-redux';
+import {createMuiTheme} from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
+import {ThemeProvider} from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 import store from './store';
+import {RestaurantScreen} from './components/RestaurantScreen';
 
-import React, { Component }  from 'react';
-import RestaurantScreen from './components/RestaurantScreen';
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+  },
+});
 
 const App = () => (
   <Provider store={store}>
-     <RestaurantScreen />
-     </Provider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Where to eat</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <RestaurantScreen />
+      </Container>
+    </ThemeProvider>
+  </Provider>
 );
 
 export default App;
